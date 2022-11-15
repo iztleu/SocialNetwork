66fca942309b6741528430244ab33ae1ad5e5899
diztleu <diztleu@1cb.kz>
2022-11-15 19:55:12 +0600

    update init sql

--- db/init.sql
+++ db/init.sql
@@ -2,14 +2,29 @@ create table users
 (
     id        bigint auto_increment
         primary key,
-    username  text not null,
-    email     text not null,
-    age       int  null,
-    password  text not null,
-    name      text null,
-    surname   text null,
-    floor     text null,
-    interests text null,
-    city      text null
+    email     varchar(500) charset utf8mb3 not null,
+    age       int                          null,
+    password  text                         not null,
+    name      text                         null,
+    surname   text                         null,
+    floor     text                         null,
+    interests text                         null,
+    city      text                         null,
+    image     text                         null,
+    constraint email
+        unique (email)
+);
+
+create table article
+(
+    id          varchar(255) charset utf8mb3 null,
+    title       text                         not null,
+    description text                         null,
+    body        text                         null,
+    createdat   datetime                     null,
+    updatedat   datetime                     null,
+    authorid    bigint                       null,
+    constraint article_users_null_fk
+        foreign key (authorid) references users (id)
 );
 
